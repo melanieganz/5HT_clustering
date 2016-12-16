c13fb2231eaa03cb17d6f36adaabe209ca1aae53
Vincent Beliveau <vbeliveau@krogh.nru.dk>
2016-12-16 17:20:57 +0100

    Started looking into ICA and DictLearning across tracers features

--- myutils.py
+++ myutils.py
@@ -5,9 +5,18 @@ import struct
 from statsmodels.api import OLS,add_constant
 from os.path import join as opj
 import math
-import ipdb
+#import ipdb # Had to downgrade to 0.8.1 due to bug
+from IPython.core.debugger import Tracer
 from mayavi import mlab
 import matplotlib.pyplot as plt
+
+# Sklearn stuff
+from sklearn.decomposition import TruncatedSVD
+import sklearn.metrics.cluster as metrics
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster.hierarchical import _hc_cut # Internal function to cut ward tree, helps speed up things a lot
+from sklearn.utils import resample
+from sklearn.model_selection import KFold,StratifiedKFold
 
 def balance_svd_components(trg,src,return_balanced=False):
     nt=np.shape(trg)[1]
@@ -41,6 +50,18 @@ def vec_angle(v1,v2):
 def norm_arclen(v,n):
     return vlen(v)**2*np.arcsin(np.dot(n,v)/vlen(v))/np.dot(n,v)
 
+def norm_range(x,percentile=None):
+    if x.ndim!=1: # Until it is extended to matrices
+            raise ValueError('Input data needs to be a vector')
+    if percentile is not None: # Trime data above and below percentiles
+        if len(percentile)!=2 and percentile.dtype!=float:
+            raise ValueError('Percentile need to be specified by an array containing exactly two floats')
+        lp=np.percentile(x,percentile[0])
+        up=np.percentile(x,percentile[1])
+        x[x<lp]=lp
+        x[x>up]=up
+    return (x - x.min()) / (x.max() - x.min())
+
 def rotate3d(v,ang,axis=0,compute=True,matrix=False):
     # Simple 3d rotation, one axis at a time, to avoid confusion.
     # Could be expanded eventually, but not necessary at the time
@@ -67,6 +88,101 @@ def normv(v):
 
 def normm(data,axis=-1):
     return np.apply_along_axis(normv,axis,data)
+
+def ward_clustering(data,adjacency,mode='split',K_range=None,save_out=None,mask=None,N_iter=1000,svd=False,K_svd=None,group=None,verbose=False):
+    # Perform Ward clustering on BPnd data
+
+    if K_range is None:
+        raise ValueError('Range of cluster number not specified')
+    if svd:
+        svd=TruncatedSVD(n_components=K_svd,algorithm='arpack')
+
+    if mode=='whole':
+        if verbose:
+            print('Performing ward clustering of the whole dataset')        
+        
+        # Apply func to the data
+        if data.dtype==object:
+            data=np.column_stack(data)
+        if svd:
+            svd.fit(np.transpose(data))
+            fdata=np.transpose(svd.components_)
+        else:
+            fdata=data
+            
+        ward_labels=np.zeros([fdata.shape[0],len(K_range)])
+        for k,nk in zip(K_range,np.arange(0,len(K_range))):
+            mdl=AgglomerativeClustering(n_clusters=k, connectivity=adjacency,linkage='ward')
+            mdl.fit(fdata)
+            ward_labels[:,nk]=mdl.labels_+1
+
+        if save_out is not None:
+            if mask is None:
+                raise ValueError('Specifiy a surface mask to save data')
+            fs_save_surf_data(ward_labels,save_out,mask=mask,verbose=verbose)
+
+    # Evaluate clustering stability for a range of K
+    elif mode=='split':
+        if verbose:
+            print('Performing split-half evaluation of clustering')
+        
+        ars=np.empty([N_iter,len(K_range)])
+        ami=np.empty([N_iter,len(K_range)])
+        mdl1=AgglomerativeClustering(compute_full_tree=True, connectivity=adjacency,linkage='ward')
+        mdl2=AgglomerativeClustering(compute_full_tree=True, connectivity=adjacency,linkage='ward')
+        if data.dtype==object:
+            N=len(data)
+        else:
+            N=data.shape[1]
+        
+        for ni in np.arange(0,N_iter):
+            # Compute splits
+            if group is None:
+                kf=KFold(n_splits=2,shuffle=True,random_state=None) # Split-half model
+                split1,split2=kf.split(np.arange(0,N))
+            else:
+                kf=StratifiedKFold(n_splits=2,shuffle=True,random_state=None) # Split-half model
+                split1,split2=kf.split(np.arange(0,N,group))
+            
+            # Apply SVD
+            if data.dtype==object:
+                data1=np.column_stack(data[split1[0]])
+                data2=np.column_stack(data[split2[0]])
+            else:
+                data1=data[:,split1[0]]
+                data2=data[:,split2[0]]
+            if svd:
+                
+                svd.fit(np.transpose(data1))
+                fdata1=np.transpose(svd.components_)
+                svd.fit(np.transpose(data2))
+                fdata2=np.transpose(svd.components_)
+            else:
+                fdata1=data1
+                fdata2=data2
+            
+            mdl1.fit(fdata1)
+            mdl2.fit(fdata2)
+
+            for nk in np.arange(0,len(K_range)):
+                # Cut trees
+                # NOTE: labels start at 0, so add one to distinguish from medial wall
+                labels1=_hc_cut(nk,mdl1.children_,mdl1.n_leaves_)+1 
+                labels2=_hc_cut(nk,mdl2.children_,mdl2.n_leaves_)+1
+
+                # Compute metrics
+                ars[ni,nk]=metrics.adjusted_rand_score(labels1, labels2)
+                ami[ni,nk]=metrics.adjusted_mutual_info_score(labels1, labels2)
+
+        # Plot metrics
+        plt.figure(figsize=(8,2))
+        plt.subplot(1,2,1)
+        plt.plot(K_range,ars.mean(axis=0))
+        plt.title('Adjusted Rand Index')
+        plt.subplot(1,2,2)
+        plt.plot(K_range,ami.mean(axis=0))
+        plt.title('Adjusted Mutual Information')
+        plt.show()
 
 # FreeSurfer stuff
 
@@ -121,32 +237,32 @@ def fs_surf_neighborhood(fname,out_type='matrix',mask=None,save_out=None,verbose
     if mask is not None:
         vertices,_,_=fs_load_mask(mask)
     else:
-        vertice=np.unique(faces) # Unique vertices ID
+        vertices=np.unique(faces) # Unique vertices ID
+    vertices=np.sort(vertices)
 
     # Preallocate memory
     if out_type=='list':
         neigh=np.ndarray(len(vertices),dtype=object)
     else:
-        neigh=np.zeros([len(vertices),len(vertices)],dtype=bool)
+        neigh=np.zeros([len(vertices),len(vertices)],dtype=int)
 
     # Identify neighbors
-    ni=0
-    for nv in vertices:
-        if verbose and ni % 10000==0 and ni != 0:
+    if verbose:
+        print('Creating neighborhood structure')
+    for nv,ni in zip(vertices,np.arange(0,len(vertices))):
+        if verbose and ni % 5000==0 and ni != 0:
             print(str(ni) + '/' + str(len(vertices)))
-
         neigh_ids=np.unique(faces[np.sum(faces==nv,axis=1)>=1,:])
-        neigh_ids=neigh_ids[np.logical_and(neigh_ids != nv,neigh_ids in vertices)]
+        # Here we are only comparing to vertices in case a mask was provided
         if out_type=='matrix':
-            neigh[np.where(vertices in neigh_ids),:]=True
+            neigh[np.array([x in neigh_ids and x!=nv for x in vertices],dtype=bool),ni]=1
         else:
-            neigh[ni]=neigh_ids
-        ni=ni+1
+            neigh[ni]=neigh_ids[np.array([x in vertices and x!=nv for x in neigh_ids],dtype=bool)] 
 
     if save_out is not None:
         if verbose:
-            print('Saving file neighborhood structure to ' + save_out + '.' + out_type)
-        np.save(save_out + '.' + out_type,neigh,vertices)
+            print('Saving neighborhood structure to ' + save_out + '.' + out_type)
+        np.savez(save_out + '.' + out_type,neigh,vertices)
 
     return neigh,vertices
 
@@ -158,26 +274,25 @@ def fs_load_surf_neighborhood(fname):
 
 def fs_create_cortex_mask(subjects_dir,targ,hemi,validate=True,verbose=False,save_out=None):
 
-    cortex=fs_read_label(opj(subjects_dir,targ,'label',hemi+'.cortex.label'))
-    medial=fs_read_label(opj(subjects_dir,targ,'label',hemi+'.Medial_wall.label'))
+    if verbose:
+        print('Reading labels')
+    cortex=np.sort(fs_read_label(opj(subjects_dir,targ,'label',hemi+'.cortex.label')))
+    medial=np.sort(fs_read_label(opj(subjects_dir,targ,'label',hemi+'.Medial_wall.label')))
     N=len(cortex)+len(medial)
 
     # Make sure each cortex vertice is neighbor of at least another cortex vertice
     # This sanity check is necessary since this vertices of the medial wall have previously
     # been labeled as cortex for fsaverage, left hemisphere (e.g. fsaverage/surf/lh.pial)
     if validate:
-        neigh=fs_surf_neighborhood(opj(subjects_dir,targ,'surf',hemi+'.pial'),out_type='list',verbose=verbose)
-
         if verbose:
-            print('Validating cortex mask')
-            ni=0
+            print('Validating cortex mask, this may take a while')   
+        faces,_=fs_read_surf(opj(subjects_dir,targ,'surf',hemi+'.pial'),verbose=verbose)
         invalid=np.ndarray(0,dtype=int)
-        for nc in cortex:
+        for nc,ni in zip(cortex,np.arange(0,len(cortex))):
             if verbose:
                 if ni % 10000==0 and ni != 0:
                     print(str(ni) + '/' + str(len(cortex)))
-                ni=ni+1
-            if np.sum([vert in cortex for vert in neigh[nc]])<2: # Require at least two neighbors in cortex
+            if np.sum([nv in cortex for nv in np.unique(faces[np.sum(faces==nc,axis=1)>=1,:])])<3: # Require at least two neighbors in cortex
                 if verbose:
                     print('Detected invalid vertex ' + str(nc))
                 np.append(invalid,nc)
@@ -186,7 +301,7 @@ def fs_create_cortex_mask(subjects_dir,targ,hemi,validate=True,verbose=False,sav
 
     if save_out is not None:
         if verbose:
-            print('Saving file ' + save_out)
+            print('Saving masks to ' + save_out)
         np.savez(save_out,cortex,medial,N)
 
     return cortex,medial,N
@@ -209,34 +324,40 @@ def fs_load_surf_data(fname,mask=None):
     else:
         return img
 
-def fs_save_surf_data(data,fname,mask=None,verbose=True):
+def fs_save_surf_data(data,fname,mask=None,verbose=False):
     if mask is not None:
-        # Here we assume that the datta is provided as a 1D or 2D matrix and that the rows correspond to the masks indices concatenated
+        # Here we assume that the data is provided as a 1D or 2D matrix and that the rows correspond to the masks indices concatenated
         if type(mask) is not list and type(mask) is not str:
-            raise ValueError('mask must be a string or a list')
+            raise ValueError('Mask must be a string or a list')
+        if type(fname) is not list and type(fname) is not str:
+            raise ValueError('Fille name must be a string or a list')
         if type(mask) is str:
             mask=[mask]
+        if type(fname) is str:
+            fname=[fname]
+        if len(mask)!=len(fname):
+            raise ValueError('Number of output files and masks is not the same')
+        
         nstart=0
         nstop=0
-        for fmask in mask:
+        for fout,fmask in zip(fname,mask):
             nstart=nstop
             ma,_,N=fs_load_mask(fmask)
-            nstop=nstop+N
-            img=np.zeros(1,N,1,data.shape[1])
-            img[0,ma,0,:]=data[np.arange(ntart,nstop),:]
+            nstop=nstop+len(ma)
+            img=np.zeros([1,N,1,data.shape[1]],dtype=float)
+            img[0,ma,0,:]=data[np.arange(nstart,nstop),:]
             if verbose:
-                print('Input dimensions=' + str(data.shape)+', Output dimensions=' + str(img.shape))
-                print('Saving surface data to file ' + fname)
-            nib.save(nib.Nifti1Image(img, np.eye(4)), fname)
+                print('Saving surface data to file ' + fout)
+            nib.save(nib.Nifti1Image(img, np.eye(4)), fout)
     else:
-        if data.ndim==1: # Here we assume that the data is well formated as a 2D matrix, i.e. there is as many rows as surface vertices
+        # Here we assume that the data is well formated as a 2D matrix, i.e. there is as many rows as surface vertices
+        if data.ndim==1:
             img=np.reshape(data,[1,data.shape[0],1,1])
-        elif data.ndim==2: # Here we assume that the data is well formated as a 2D matrix, i.e. there is as many rows as surface vertices
+        elif data.ndim==2:
             img=np.reshape(data,[1,data.shape[0],1,data.shape[1]])
         else:
             img=data
         if verbose:
-            print('Input dimensions=' + str(data.shape)+', Output dimensions=' + str(img.shape))
             print('Saving surface data to file ' + fname)
         nib.save(nib.Nifti1Image(img, np.eye(4)), fname)
 
@@ -267,8 +388,7 @@ def fs_surf_gradient_struct(fname,mask,verbose=False,validate_rotation=False,sav
 
     if verbose:
         print('Computing gradient structure for cortical vertices')
-        ni=0
-    for nc in cortex:
+    for nc,ni in zip(cortex,np.arange(0,len(cortex))):
 
         # Note: vertices for each faces are always ordered so that their cross product points outward,
         # hence to have consistent normals, we simply need to make sure the ordering is respected.
@@ -276,13 +396,11 @@ def fs_surf_gradient_struct(fname,mask,verbose=False,validate_rotation=False,sav
         if verbose:
             if ni % 10000==0 and ni != 0:
                 print(str(ni) + '/' + str(len(cortex)))
-            ni=ni+1
 
         # Find the normal of each face the current vertice is part of and take the average
         faces_ind=np.where(np.sum(faces==nc,axis=1)>=1)[0] # Extract rows for each faces containing the current vertice
         fnorm=np.ndarray([len(faces_ind),3])
-        nn=0
-        for nf in faces_ind:
+        for nf,nn in zip(faces_ind,len(faces_ind)):
             ind=np.where(faces[nf,:]==nc)[0]
             if ind==0:
                 fnorm[nn,:]=np.cross(position[faces[nf,2],:]-position[faces[nf,0],:],
@@ -293,8 +411,6 @@ def fs_surf_gradient_struct(fname,mask,verbose=False,validate_rotation=False,sav
             elif ind==2:
                 fnorm[nn,:]=np.cross(position[faces[nf,1],:]-position[faces[nf,2],:],
                                 position[faces[nf,0],:]-position[faces[nf,2],:])
-
-            nn=nn+1
 
         # Take average of all faces norms and normalize final vector
         snorm=normv(normm(fnorm,axis=1).mean(axis=0))
@@ -361,7 +477,7 @@ def fs_surf_gradient(data,fgrad,save_out=None,verbose=False):
             ipdb.set_trace()
             return
 
-    # Border vertuces take the mean gradient of their neighbors
+    # Border vertices take the mean gradient of their neighbors
     if verbose:
         print('Processing border vertices')
     for nb in border:
@@ -412,13 +528,10 @@ def fs_surf_view(surf,data=None,view='mid',hemi='lh',snap=False,plot_snap=False)
             mlab.view(azimuth=-90,elevation=90,distance=dist)
         else:
             raise ValueError('Invalid view '+str(view[nv]))
-
-        mlab.show()
-        print(mlab.view())
-
         if snap:
             img[nv]=mlab.screenshot()
-            mlab.close()
+        else:
+            mlab.show()
 
     if snap:
         if plot_snap:
